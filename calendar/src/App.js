ff0d067d477493904f44a0ff436faff2126db562
Chiranjit Behera <chiranjitbehera34@gmail.com>
2022-09-05 23:11:01 +0530

    init

---
+++ calendar/src/App.js
@@ -0,0 +1,36 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calendar</h1>
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
+    </div>
+  );
+}
+
+export default App;
